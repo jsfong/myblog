922845629e0be7cb1c79cdc5584f0a37d6fb4a29
Jsfong <jsfong5105@gmail.com>
2019-06-23 13:48:49 +0800

    Add Data Validation to Rest controller

--- src/main/java/com/jsfong/myblog/controllers/BlogRestController.java
+++ src/main/java/com/jsfong/myblog/controllers/BlogRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jsfong.myblog.entities.BlogRequireFieldNotFoundException;
 import com.jsfong.myblog.entities.Blogpost;
 import com.jsfong.myblog.service.BlogService;
 
@@ -23,9 +24,12 @@ public class BlogRestController {
 	BlogService service;
 
 	// Create
-	//TODO Validation - Title is mandatory
 	@PostMapping
 	public Blogpost createBlog(@RequestBody Blogpost post) {
+		
+		if(post.getTitle() == null) {
+			throw new BlogRequireFieldNotFoundException("Blog title should not be empty");
+		}
 		return service.createBlogEntry(post);
 
 	}
@@ -43,10 +47,21 @@ public class BlogRestController {
 	}
 
 	// Update
-	//TODO Validation-able to update partial field
 	@PutMapping("{id}")
-	public Blogpost updateBlog(@PathVariable("id") int id, @RequestBody Blogpost post) {		
-		return service.updateBlogEntry(post);
+	public Blogpost updateBlog(@PathVariable("id") int id, @RequestBody Blogpost post) {	
+
+		Blogpost blog2Update = service.getBlogEntryById(id);
+	
+		//Update Title if not null
+		if(post.getTitle() != null) {
+			blog2Update.setTitle(post.getTitle());
+		}
+		
+		//Update Body if not null
+		if(post.getBody() != null) {
+			blog2Update.setBody(post.getBody());
+		}
+		return service.updateBlogEntry(blog2Update);
 	}
 
 	// Delete
---
+++ src/main/java/com/jsfong/myblog/entities/BlogRequireFieldNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jsfong.myblog.entities;
+
+
+@SuppressWarnings("serial")
+public class BlogRequireFieldNotFoundException extends RuntimeException {
+
+	public BlogRequireFieldNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+}
