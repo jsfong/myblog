dac002d535c332fb161c399d433138b5e225af02
Fong Jiunn Shian <jifong@gemalto.com>
2019-06-26 14:23:18 +0800

    Rename step file

--- src/test/java/com/jsfong/myblog/integration/BlogStep.java
+++ src/test/java/com/jsfong/myblog/integration/BlogStep.java
@@ -16,7 +16,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class CreateBlogStep extends CucumberRoot {
+public class BlogStep extends CucumberRoot {
 
 	final String uri = "/blog";
 	private Blogpost samplePost;
